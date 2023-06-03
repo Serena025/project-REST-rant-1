59425086478942ee99512819093bfff514eff238
DantheDeveleoper <119366607+DantheDeveleoper@users.noreply.github.com>
2023-06-03 09:42:45 -0700

    Fixed home

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.render('home')
+})
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
 
-app.get('/', (req, res) => {
-    res.render('places/index', {places})
-})
 
 app.get('*', (req, res) => {
     res.render('error404')
